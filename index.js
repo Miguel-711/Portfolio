79873a5456d61f8973eae85794ae64af70eb1747
Miguel01198 <gonzalez98m@gmail.com>
2025-03-09 17:00:37 -0700

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+// let userName;
+
+// document.getElementById("mySubmit").onclick = function() {
+//     userName = document.getElementById("myText").value;
+//     document.getElementById("myTitle").textContent = `Welcome ${userName}`;
+// }
