9e9e6ca0b4158492d1aa499a16329b519210aa7d
Miguel01198 <gonzalez98m@gmail.com>
2025-03-21 16:23:46 -0700

    Refactor styles in indexStyle.css, remove unused index.js, and add media queries for responsive design

--- index.js
+++
@@ -1,6 +0,0 @@
-// let userName;
-
-// document.getElementById("mySubmit").onclick = function() {
-//     userName = document.getElementById("myText").value;
-//     document.getElementById("myTitle").textContent = `Welcome ${userName}`;
-// }
